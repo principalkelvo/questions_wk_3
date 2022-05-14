3bb5d86ac641f61d08d6fc6ab4e3d97b23a473f5
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-14 17:29:21 +0300

    adds event listener for keys capslock, backspace and alphabets working perfectly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,32 @@ let keys = document.querySelectorAll('.container .key .row span'),
 
 
 
+    if (keys && keyPad && display) {
+        let capsLockMode = false;
+        keys.forEach(key => {
+            //event listener for each key
+            key.addEventListener('click', function () {
+                console.log(this.innerText);
+                //check if caps lock is on
+                if (this.classList.contains('caps')) {
+                    this.classList.toggle('active');
+                    keyPad.classList.toggle('uppercase');
+    
+                    capsLockMode ? capsLockMode = false : capsLockMode = true;
+                    //checks backspace
+                } else if (this.classList.contains('backspace')) {
+                    let str = display.innerText;
+                    display.innerText = str.substring(0, (str.length - 1));
+                    //changes keys to uppercase
+                } else {
+                    if (capsLockMode) {
+                        display.innerText += this.dataset.key.toUpperCase();
+                    } else {
+                        display.innerText += this.dataset.key.toLowerCase();
+                    }
+                }
+            });
+        });
+        console.log(keys);
+    
+    }
