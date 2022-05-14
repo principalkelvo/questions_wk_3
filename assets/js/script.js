a92c3a622967226f2071b16319376dfa1dfd26f3
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-14 17:24:24 +0300

    adds selectors for html elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let keys = document.querySelectorAll('.container .key .row span'),
+    keyPad = document.querySelector('.container .key'),
+    display = document.querySelector('.container .display');
+
+
+
