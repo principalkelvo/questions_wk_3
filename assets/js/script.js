db0db0e0e0db95fd7500f96d13f8a8b96697c058
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-15 11:09:00 +0300

    created an array for questions and answers checks answers and awards scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,55 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        correct: "javascript",
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        correct: "eslint",
+    },
+    {
+        question: "What was the first name of JavaScript?",
+        correct: "mocha",
+    },
+    {
+        question: "Who invented Javascript?(first and second name)",
+        correct: "brendan eich",
+    },
+    {
+        question: "Javascript File has an extension of .__",
+        correct: "js",
+    },
+
+
+];
+
+
 let keys = document.querySelectorAll('.container .key .row span'),
     keyPad = document.querySelector('.container .key'),
     display = document.querySelector('.container .display');
 var submit = document.querySelector('.container .submitBtn');
+var preElements = document.getElementsByTagName("pre");
 var textBox = document.querySelector('.container .userAnswer');
+var quiz = document.querySelector('.container .question');
 var sentence = "";
 var userAnswer= '';
-var preElements = document.getElementsByTagName("pre");
+var currentQuiz=0;
+var score=0;
+
+loadQuiz()
 
 submit.addEventListener('click', submitForm);
+console.log('hello'+textBox.innerText+'hello');
+if(textBox.innerText == ''){
+    textBox.classList.add('hide');
+}
 
-
+async function loadQuiz(){
+    quiz.innerHTML = quizData[currentQuiz].question;
+    // for(let i=0; i<quizData.length; i++){
+    //     document.getElementById(i).innerHTML = quizData[currentQuiz][i];
+    // }
+}
 
 async function getData(){
     if (keys && keyPad && display) {
@@ -51,8 +91,31 @@ getData();
 //submit user answer
 async function submitForm() {
     textBox.innerHTML = sentence;
-    userAnswer = textBox.innerHTML;
+    userAnswer = textBox.innerHTML.toLowerCase();
+    textBox.classList.remove('hide');
+console.log('hello'+textBox.innerText+'hello');
     display.innerHTML= '';
+    if(userAnswer == quizData[currentQuiz].correct){
+        score++;
+        console.log(score);
+    }
+    else{
+        console.log('wrong');
+    }
+    setTimeout(function(){
+    currentQuiz++;
+    if(currentQuiz < quizData.length){
+        loadQuiz();
+    textBox.classList.add('hide');
+
+    }
+    else{
+        console.log('game over');
+        currentQuiz=0;
+    }
+    },3100)
+    
+
     clear();
 }
 
